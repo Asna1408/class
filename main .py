50833c65229313060c312de0d8667d4284afa1d7
Asna Majeed <85230276+Asna1408@users.noreply.github.com>
2023-02-13 16:45:04 +0530

    Update main .py

--- main .py
+++ main .py
@@ -1,1 +1,9 @@
+class Student:
+    def __init__(self, name, place, age):
+        self.name = name
+        self.place = place
+        self.age = age
+    
+    def get_age(self):
+        self.age= age
 
