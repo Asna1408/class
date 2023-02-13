bfcfd06f62b632fc1361cba9576006a0ca970e07
Asna Majeed <85230276+Asna1408@users.noreply.github.com>
2023-02-13 21:42:03 +0530

    Update main .py

--- main .py
+++ main .py
@@ -1,20 +1,8 @@
-
-import class
-class Student:
-    def __init__(self, name, place, age):
-        self.name = name
-        self.place = place
-        self.age = age
-    
-    def get_age(self):
-        self.age= age
-
-    def set_age(self):
-        print(self.age)
-    
-    def display_student_details(self):
-        print("Name: ", self.name)
-        print("Place: ", self.place)
-        print("Age: ", self.age)
-
+import class1
+s = class1.Student("Asna Majeed","21","Kochi")
+s.display_student_details()
+s.get_age()
+s.set_age("19")
+s.get_age()
+s.display_student_details()
 
